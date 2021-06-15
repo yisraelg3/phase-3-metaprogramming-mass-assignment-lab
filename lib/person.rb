0540082de395e23cdf2c67acf161027d37e85b3a
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-14 22:38:19 -0400

    commit tree fed2a30359e4f8c5146ad3c15a857cc16c214ccb

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-  #your code here
+  def initialize(attribute)
+    attribute.each{|key, value| 
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)}
+      
+  end
 end
+  
